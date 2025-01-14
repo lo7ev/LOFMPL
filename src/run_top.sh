7881009ab2e484f2a5481c43c74ba8e40ddd8c8b
kxzhu0505 <97582446+kxzhu0505@users.noreply.github.com>
2025-01-14 20:13:58 +0800

    Update run_top.sh

--- src/run_top.sh
+++ src/run_top.sh
@@ -6,15 +6,15 @@ bsub -I 'python3 ./top.py \
 -merge_py ./merge.py \
 -rl_yqian_P_opt_py ./rl_yqian_P_opt.py \
 -benchs_info_file ./benchinfo/bench_info.yaml \
--yosys_exe /home/wllpro/llwang04/yosys-yosys-0.32/yosys \
--abc_exe /home/wllpro/llwang04/abc-master/abc \
--abc_p_exe /home/wllpro/llwang04/zli/logic_synthesis/partition_adaptive_mffc_kahypar_n10000/abc_p/abc \
--lsoracle_exe /home/wllpro/llwang04/zli/logic_synthesis/partition_adaptive_mffc_kahypar_n10000/LSOracle/build/core/lsoracle \
+-yosys_exe ../yosys/yosys \
+-abc_exe ../abc_p/abc \
+-abc_p_exe ../abc_p/abc \
+-lsoracle_exe ../LSOracle/build/core/lsoracle \
 -part_size 10000 \
 -abc_netlist_plugin /home/wllpro/llwang04/zli/logic_synthesis/partition_n10000_rl_yqian_merge/abc_netlist/abc_netlist.so \
 -synopsys_dc_setup_file /home/wllpro/llwang04/zli/logic_synthesis/orig_dc/.synopsys_dc.setup \
--outputs_dir_monitor /home/wllpro/llwang05/zli/logic_synthesis/partition_n10000_rl_yqian_opt/outputs \
--outputs_dir_noop /home/wllpro/llwang05/zli/logic_synthesis/partition_n10000/outputs \
--rl_logic_synthesis_dir /home/wllpro/llwang04/zli/logic_synthesis/rl_yqian_P_opt/rl_logic_synthesis/ \
--outputs_dir_all_parts ./outputs \
+-outputs_dir_monitor ../outputs \
+-outputs_dir_noop ../outputs \
+-rl_logic_synthesis_dir ../rl_logic_synthesis/ \
+-outputs_dir_all_parts ../outputs \
 -process 100'
