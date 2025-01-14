a84ff7e1ed69297836e57d37db2d766424014aae
kxzhu0505 <97582446+kxzhu0505@users.noreply.github.com>
2025-01-14 20:09:26 +0800

    Delete Scripts directory

--- Scripts/run_DSE.sh
+++
@@ -1,7 +0,0 @@
-python3 ./dse.py \
--benchs_info_file ../benchs_info/benchs_info_all.yaml \
--rl_logic_synthesis_dir ../rl_logic_synthesis \
--outputs_dir_all_parts ./outputs \
--abc_exe ../abc-master/abc \
--process 50
-
--- Scripts/run_partition.sh
+++
@@ -1,8 +0,0 @@
-python3 ./partition.py \
--opt_py ./opt_partition.py \
--benchs_info_file ../benchs_info/benchs_info_all.yaml \
--yosys_exe ../yosys-yosys-0.32/yosys \
--abc_exe ../abc-master/abc \
--abc_p_exe ../abc_p/abc \
--lsoracle_exe ../LSOracle/build/core/lsoracle \
--part_size 10000
